0d07a8d057517529a7d700fae2f8e2f55b09379d
shimaa hamdy <shimaahmady722@gmail.com>
2022-04-20 04:50:54 +0200

    overl all structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { SideMenuComponent } from './Components/side-menu/side-menu.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    SideMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
